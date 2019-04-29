69913bb7f440f11d9ee2403e1b981d19dc100ac7
screeeen <miguel@screenimplosion.com>
2019-04-29 23:57:01 +0200

    iteration 5

--- mongoose-movies/routes/celebrities.js
+++ mongoose-movies/routes/celebrities.js
@@ -12,15 +12,35 @@ router.get('/', (req, res, next) => {
     .catch(err => console.log(err));
 });
 
-// GET '/celebrities'
+// GET '/celebrities query'
 router.get('/celebrities/:id', (req, res, next) => {
-  const  _id  = req.params.id;
+  const _id = req.params.id;
 
-  console.log('id: ',_id);
+  console.log('id: ', _id);
   Celebrity.findById(_id)
     .then(celebrity => res.render('show', { celebrity }))
     .catch(err => console.log(err));
   // If there's an error, call the route's next function and return the error. //HOW??
+});
+
+// GET ./celebrities/new
+router.get('/new', function(req, res, next) {
+  res.render('new');
+});
+
+//Post '/celebrities/new'
+router.post('/new', function(req, res, next) {
+  console.log('req.body', req.body); // my notes: because is post, stores into body. Post has body, GET query or params
+  // my notes: fetching the var or deconstruction
+  // const title = req.body.title;
+  //or deconstruction
+  const { name, occupation, catchPhrase } = req.body; //deconstruct from form action, body etc
+
+  const newCeleb = new Celebrity({ name, occupation, catchPhrase }); //instantiate the object
+  newCeleb
+    .save() // save it into db, this format is a thenable.
+    .then(celebrity => res.redirect('/'))
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
