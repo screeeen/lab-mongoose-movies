b88d327714bebf49af52825b9602fc364c2f0c01
screeeen <miguel@screenimplosion.com>
2019-04-29 23:26:25 +0200

    iteration 3

--- mongoose-movies/app.js
+++ mongoose-movies/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const router = require('./routes/index');
+const router = require('./routes/celebrities');
 const mongoose = require('mongoose');
 
 
--- mongoose-movies/routes/celebrities.js
+++ mongoose-movies/routes/celebrities.js
@@ -4,16 +4,23 @@ const Celebrity = require('./../models/Celebrity');
 
 // GET '/celebrities'
 router.get('/', (req, res, next) => {
-  // const thing 
-  
   Celebrity.find({})
-  .then(allTheCelebritiesFromDB =>{
-    
-      console.log('anything?', allTheCelebritiesFromDB);
-      res.render('celebrities', { allTheCelebritiesFromDB })
-    }
-    )
+    .then(allTheCelebritiesFromDB => {
+      // console.log('anything?', allTheCelebritiesFromDB);
+      res.render('celebrities', { allTheCelebritiesFromDB });
+    })
     .catch(err => console.log(err));
+});
+
+// GET '/celebrities'
+router.get('/celebrities/:id', (req, res, next) => {
+  const  _id  = req.params.id;
+
+  console.log('id: ',_id);
+  Celebrity.findById(_id)
+    .then(celebrity => res.render('show', { celebrity }))
+    .catch(err => console.log(err));
+  // If there's an error, call the route's next function and return the error. //HOW??
 });
 
 module.exports = router;
